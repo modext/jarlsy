5fa904a26f657a230470e43e763b30185aca444b
Your Name <33578391+modext@users.noreply.github.com>
2023-03-08 15:02:30 +0100

    product package

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -3,15 +3,23 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDrrB7r8XEIUqvb1vr1iAcGyQE60IvKKMY",
-  authDomain: "jarlsy.firebaseapp.com",
-  projectId: "jarlsy",
-  storageBucket: "jarlsy.appspot.com",
-  messagingSenderId: "83454434007",
-  appId: "1:83454434007:web:10caf11b3e323b0e40ee92"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDrrB7r8XEIUqvb1vr1iAcGyQE60IvKKMY",
+//   authDomain: "jarlsy.firebaseapp.com",
+//   projectId: "jarlsy",
+//   storageBucket: "jarlsy.appspot.com",
+//   messagingSenderId: "83454434007",
+//   appId: "1:83454434007:web:10caf11b3e323b0e40ee92"
+// };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB-dOKoHBLC1VEpJDNzt22raubJBfc2u9U",
+  authDomain: "brance-dev.firebaseapp.com",
+  projectId: "brance-dev",
+  storageBucket: "brance-dev.appspot.com",
+  messagingSenderId: "674533390838",
+  appId: "1:674533390838:web:bcb7accc26504276146fd7"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
---
+++ permissions.js
@@ -0,0 +1,14 @@
+export const serviceAccount = {
+  type: 'service_account',
+  project_id: 'brance-dev',
+  private_key_id: '0815ac6f302255edf23b7870e457c990f59c867c',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDQC8tdkZKh0Oqe\njFL70aESHKkIcdYyBjYMFW7ktKP1V96n8aKTb8T+FCYw1hlBbX+zx9RWUKUYFxml\n9OYBMjYXgBF/OOiMdnsu0u6+uCDLiCJMNo4DQDg9/OzwTOj4XyLE99mY9cU7vyzl\nlEG1VKvYHxDQ+rhdZjUA6MBhBZLfp3tIC1TUi49n9lGNG3uFTWmPrZ/Yzv96fsAo\nvJ4jl23nuRAZulBpkzhiFxVI+R5LWHYWm0fH+i5VdD/FQq7OtXb9tWPxwYur7HjG\n+hhwm/+nqPBSYC1LzT80nNpTPXt5Fv5nWYy06yM/Wc91oSs95thMELOpuYyTy9cB\nmKpuBnp1AgMBAAECggEALmGuw0vAw8QahmHU31C/JtVOZvRQE0N4ua1ulVzjX79p\nA4qWmuWUZquxzvnwU/qYOYmvoFWm1M3TmUDVpUcfMZKK9BDJIYWJlWkPsmPE5fNR\n6ClSdE2VLFausS0IVFULYJu7/oKuZ9FJbl5k3ssDa7GTU2/N+f6fCvaY6xhtw+AA\n+zpiRI6RQnl1ZPRXBcgDx60N4Vz/9XKGkWbJTXHjv3G7/VtI0941IOfDWBKbO8JP\nK/u6T06v6L5Txk7ed0bfN//zMD4suGmFBgnglck3nwBbqh/11XHdZCbS8en3uS7W\nPDlgKa/chNshMXGAVmxGtA37JF0GFSZQOKqdmCCQIQKBgQD17l/NPRYhqYINxsUz\nC/5DO9VBaE8cKgiH7qX4kh/njrDyccFF+hGwzTwu754xS1ynmxqxZPRzJobwu/LD\n9QwzOrDs2OEPrir8YuJMpwgDwnJj5kSQI2Gnrb915UeAbaX3r9lzzL0NK6jhWlUQ\nnHqI/LIVhxZrxbqTiOiTULEQFQKBgQDYkFfnnBHYTtx75+mf+A219tPIHdPC6NUC\nhVhewGzUOot81/jcwTdV5dB5gCTYXLXQe8aQBGX+eaoeUf4Z3cA5+5UH0fLREsgW\nk+tQ4Y4bcE+ff7vlQEolgsCV5Hss7gEs//d7IEqtbmaiZUcbA1JVCuAcMyFzANib\nLeRfsUf44QKBgARdPY/71Ie2ZVtvOpKkVNfGmQs3GahLSql237UgirZmPteekyp1\nvJihm77Ne8YuKy/CPeAGBZyBg4WEIFO/5n6GPRuG4b+QfKzL6G4pzKecbME/Fdpk\n9leuaqZh8jniOD7D/Rzj9jrkmnOdUcMgs7/4lARzyD8TXDNAC7iJhwrdAoGAEKvD\n6JNuGD2x4WPv6x2YSJqwoOYS6B0uUJxdOiYL+qZrGHgzsyK4lMc1LjU8IN+6K0tr\n9KgyBc3t15muOuWEJ7KDPW/fBlMwzfQjGD3Ux630oy2dFD/HtW2ET8Wf6cyEkKc/\nsN3CgelOw0+SkrTFwMAukl0pRQYapo941hj6lmECgYBicZKfIpzk4Y8X/H7ogisQ\n0+3dbyuL8Qz20clD0GYsyfk3t85wbl9wvB3BYYE4IsvIjiq/ETxVEo9reY0C/zqZ\nuemXnsSdv0VsjUjdSGKwheE+k0m5FjoMbAN09HsAK9bPCJ1XjWozLOi1R6iUb6m4\nLHi//+m4PW2aXRa9Y4kw2g==\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-gvbmy@brance-dev.iam.gserviceaccount.com',
+  client_id: '100872943746784572292',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gvbmy%40brance-dev.iam.gserviceaccount.com',
+};
