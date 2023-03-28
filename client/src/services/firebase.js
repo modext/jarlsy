7c02236b96fa3be1c3670d58392fe08ae7cee7c8
Your Name <33578391+modext@users.noreply.github.com>
2023-03-28 10:31:52 +0100

    settings

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore();
+const db = getFirestore(app);
 const auth = getAuth();
 const storage = getStorage();
 
